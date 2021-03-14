d5b5c1560dfaa07cfc5977027b6923d6e79d1dec
Keks <keks@htmlacademy.ru>
2021-03-14 23:18:17 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/card.js
@@ -0,0 +1,17 @@
+const buyLink = document.querySelectorAll(".buy");
+const buyPopup = document.querySelector(".modal-add");
+const buyClose = buyPopup.querySelectorAll(".modal-busket-close");
+
+
+buyLink.forEach(function(item){
+    item.addEventListener('click', function(evt) {
+      evt.preventDefault();
+      buyPopup.classList.add("modal-show");
+    });
+});
+  
+  buyClose.forEach(function(item){
+    item.addEventListener('click', function(e) {
+      buyPopup.classList.remove("modal-show");
+    });
+});
---
+++ 12/js/mapFeedback.js
@@ -0,0 +1,55 @@
+
+
+const mapLink = document.querySelector(".map");
+const mapPopup = document.querySelector(".modal-map");
+const mapClose = mapPopup.querySelector(".close-map");
+
+const contactLink = document.querySelector(".contact");
+const contactPopup = document.querySelector(".modal-feedback");
+const contactClose = contactPopup.querySelector(".modal-close");
+const userName = contactPopup.querySelector ("[name=name]")
+
+
+mapLink.addEventListener("click", function () {
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function () {
+  mapPopup.classList.remove("modal-show");
+});
+
+
+contactLink.addEventListener("click", function () {
+  contactPopup.classList.add("modal-show");
+  userName.focus();
+});
+
+contactClose.addEventListener("click", function () {
+  contactPopup.classList.remove("modal-show");
+});
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      mapPopup.classList.remove("modal-show");
+    }
+  }
+
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (contactPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      contactPopup.classList.remove("modal-show");
+    }
+  }
+
+});
+
+
+
+
+
